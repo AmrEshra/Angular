5d2214a4d1bb16bb226e225c7fb95263d7656b43
Amr Eshra <amr.eshra@gmail.com>
2020-06-14 17:01:29 +0300

    .

--- helloWorld/src/Shapes/Testing.ts
+++ helloWorld/src/Shapes/Testing.ts
@@ -2,7 +2,7 @@ import {Shape} from './Shape';
 import {Rectangle} from './Rectangle';
 import {Circle} from './Circle';
 
-// let s = new Shape(2, 3);
+// let s = new Shape(2, 3); 
 let r = new Rectangle(0, 0, 8, 10);
 let c = new Circle(5, 5, 100);
 
